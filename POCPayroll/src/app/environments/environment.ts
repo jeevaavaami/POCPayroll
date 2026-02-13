5c3ce7e8453e9fbba5442dda38db5ba5d4577f61
Jeeva <jeeva071182@gmail.com>
2026-02-13 22:33:49 +0530

    Code Push

---
+++ POCPayroll/src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyC7CWziP7PeHH_ZeC-wmEFKzC1zB76gD4Q",
+  authDomain: "avaamipayroll-f0b70.firebaseapp.com",
+  projectId: "avaamipayroll-f0b70",
+  storageBucket: "avaamipayroll-f0b70.firebasestorage.app",
+  messagingSenderId: "913971527348",
+  appId: "1:913971527348:web:3b48b74c52b02b5dd4e1e1",
+  measurementId: "G-DW58K3LQRC"
+  }
+};
